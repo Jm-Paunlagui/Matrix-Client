5641f7126942faeba15599b3adc70bdc591d0dde
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2023-02-03 18:01:42 +0800

    stressfull

--- client/src/http/httpClient.js
+++ client/src/http/httpClient.js
@@ -8,7 +8,6 @@ export default axios.create({
   baseURL: "http://localhost:5000",
   headers: {
     "Content-type": "application/json",
-    "Access-Control-Allow-Origin:": "*",
     "Access-Control-Expose-Headers:": "Content-Disposition",
   },
 });
